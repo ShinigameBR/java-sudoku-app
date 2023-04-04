4cae16469f6f532d556e4e0ec88998d317ad1722
ShinigameBR <shinigamecontato@gmail.com>
2023-04-04 18:49:11 -0300

    User Interface

---
+++ src/sudoku/constants/Messages.java
@@ -0,0 +1,6 @@
+package sudoku.constants;
+
+public class Messages {
+    public static final String GAME_COMPLETE = "Parabéns, Você ganhou! Deseja jogar novamente?";
+    public static final String ERROR = "Algo de errado não está certo!";
+}
